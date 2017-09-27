5bc36770bd3151f632163e70ec4d6d97f5d3b841
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-09-27 13:39:06 -0400

    practice with age if-else

--- Documents/visual studio 2015/Projects/Week1_Day3/Week1_Day3/Program.cs
+++ Documents/visual studio 2015/Projects/Week1_Day3/Week1_Day3/Program.cs
@@ -28,23 +28,61 @@ namespace Week1_Day3
             //}
 
 
-            Console.WriteLine("What day is today?");
+            //Console.WriteLine("What day is today?");
 
-            string userAnswer = Console.ReadLine();
+            //string userAnswer = Console.ReadLine();
 
-            if (userAnswer == "Monday") ;
+            //if(userAnswer == "Monday") ;
+            //{
+            //    Console.WriteLine("Back to work!");
+            //}
+
+
+            //else if(userAnswer == "Friday") ;
+
+            //{
+            //    Console.WriteLine("TGIF");
+            //}
+
+            Console.WriteLine("How old are you?");
+            int userAge = int.Parse(Console.ReadLine());
+
+            if (userAge <= 1)
             {
-                Console.WriteLine("Back to work!");
+                Console.WriteLine("You're just a baby!");
+            }
+            else if (userAge > 1 && userAge < 4)
+            {
+                Console.WriteLine("Yay todlers!");
+            }
+              else if(userAge >= 4 && userAge < 13)
+            {
+                Console.WriteLine("Enjoy your childhood!");
+            }
+            else if(userAge > 13 && userAge < 65)
+            {
+                Console.WriteLine("Life is akward!");
+            }
+            else if(userAge >= 65 && userAge < 100)
+            {
+                Console.WriteLine("Enjoy your retirement!");
+            }
+            else 
+            {
+                Console.WriteLine("That's REALLY not your age!");
             }
 
-            else if (userAnswer == "Friday") ;
 
-            {
-                Console.WriteLine("TGIF");
-            }
+
+
+
+
+
+
 
                 
-                )
+            
+
 
         }
     }
