50b4b8bea2c9be3ac6789f91d80415b4fd59c3af
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-09-27 13:09:07 -0400

    started with if-else

--- Documents/visual studio 2015/Projects/Week1_Day3/Week1_Day3/Program.cs
+++ Documents/visual studio 2015/Projects/Week1_Day3/Week1_Day3/Program.cs
@@ -10,6 +10,42 @@ namespace Week1_Day3
     {
         static void Main(string[] args)
         {
+
+
+            //int firstNumber = 2;
+            //int secondNumber = 6;
+
+            //if (firstNumber > secondNumber)
+            //{
+            //    // IF the statement above is TRUE...do something
+            //    Console.WriteLine("Try again!");
+            //}
+
+            //else
+            //{ 
+            //    // do something else
+            //    Console.WriteLine("Good job!");
+            //}
+
+
+            Console.WriteLine("What day is today?");
+
+            string userAnswer = Console.ReadLine();
+
+            if (userAnswer == "Monday") ;
+            {
+                Console.WriteLine("Back to work!");
+            }
+
+            else if (userAnswer == "Friday") ;
+
+            {
+                Console.WriteLine("TGIF");
+            }
+
+                
+                )
+
         }
     }
 }
