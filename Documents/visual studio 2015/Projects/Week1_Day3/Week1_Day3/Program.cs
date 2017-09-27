97a085ed93d4f9c63569c69b9a508d22b562eca9
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-09-27 13:57:51 -0400

    practice with switch

--- Documents/visual studio 2015/Projects/Week1_Day3/Week1_Day3/Program.cs
+++ Documents/visual studio 2015/Projects/Week1_Day3/Week1_Day3/Program.cs
@@ -44,37 +44,67 @@ namespace Week1_Day3
             //    Console.WriteLine("TGIF");
             //}
 
-            Console.WriteLine("How old are you?");
-            int userAge = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How old are you?");
+            //int userAge = int.Parse(Console.ReadLine());
 
-            if (userAge <= 1)
-            {
-                Console.WriteLine("You're just a baby!");
-            }
-            else if (userAge > 1 && userAge < 4)
-            {
-                Console.WriteLine("Yay todlers!");
-            }
-              else if(userAge >= 4 && userAge < 13)
-            {
-                Console.WriteLine("Enjoy your childhood!");
-            }
-            else if(userAge > 13 && userAge < 65)
-            {
-                Console.WriteLine("Life is akward!");
-            }
-            else if(userAge >= 65 && userAge < 100)
-            {
-                Console.WriteLine("Enjoy your retirement!");
-            }
-            else 
-            {
-                Console.WriteLine("That's REALLY not your age!");
-            }
-
-
+            //if (userAge <= 1)
+            //{
+            //    Console.WriteLine("You're just a baby!");
+            //}
+            //else if (userAge > 1 && userAge < 4)
+            //{
+            //    Console.WriteLine("Yay todlers!");
+            //}
+            //  else if(userAge >= 4 && userAge < 13)
+            //{
+            //    Console.WriteLine("Enjoy your childhood!");
+            //}
+            //else if(userAge > 13 && userAge < 65)
+            //{
+            //    Console.WriteLine("Life is akward!");
+            //}
+            //else if(userAge >= 65 && userAge < 100)
+            //{
+            //    Console.WriteLine("Enjoy your retirement!");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("That's REALLY not your age!");
+            //}
 
 
+
+            Console.WriteLine("How many points did you earn?");
+            int pointsEarned = int.Parse(Console.ReadLine());
+
+            switch (pointsEarned)
+            {
+                case 0:
+                    Console.WriteLine("YOU LOSE!");
+
+                    break;
+                case 1:
+                    Console.WriteLine("Needs practice!");
+                    break;
+                case 2:
+                    Console.WriteLine("On target!");
+                    break;
+                case 3:
+                    Console.WriteLine("Want a cookie?");
+                    break;
+                case 4:
+                    Console.WriteLine("Overachiever!");
+                    break;
+                case 5:
+                    Console.WriteLine("CHEATER!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid score entered.");
+                    break;
+
+
+
+            }
 
 
 
